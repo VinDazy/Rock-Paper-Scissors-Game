7b9eeea950a88e5ef1f6c6662f9a974c438c0cb8
VinDazy <hectorvalinci@gmail.com>
2023-03-26 22:21:28 +0200

    Two players mode

--- index.js
+++ index.js
@@ -4,37 +4,46 @@ const result = document.getElementById('result');
 const restart = document.getElementById('restart');
 const modal = document.querySelector('.modal');
 const scoreboard={
-    player: 0,
-    computer:0
+    player_1: 0,
+    player_2:0
 }
+let currentPlayer = 1;
+let player_1_Choice, player_2_Choice;
 
 function play(e){
-    restart.style.display='inline-block'
-    const playerChoice=(e.target.id);
-    const computerChoice=getComputerChoice();
-    const winner=getWinner(playerChoice,computerChoice);
-    showWinner(winner,computerChoice)
+  restart.style.display='inline-block'
+  const choice = e.target.id;
+  if (currentPlayer === 1) {
+      player_1_Choice = choice;
+      currentPlayer = 2;
+  } else {
+      player_2_Choice = choice;
+      currentPlayer = 1;
+      const winner = getWinner(player_1_Choice, player_2_Choice);
+      showWinner(winner,player_1_Choice,player_2_Choice);
+  }
+  console.log(player_1_Choice,player_2_Choice);
 }
-function getWinner(p, c) {
-    if (p === c) {
+function getWinner(p1, p2) {
+    if (p1 === p2) {
       return 'draw';
-    } else if (p === 'rock') {
-      if (c === 'paper') {
-        return 'computer';
+    } else if (p1 === 'rock') {
+      if (p2 === 'paper') {
+        return 'player_2';
       } else {
-        return 'player';
+        return 'player_1';
       }
-    } else if (p === 'paper') {
-      if (c === 'scissors') {
-        return 'computer';
+    } else if (p1 === 'paper') {
+      if (p2 === 'scissors') {
+        return 'player_2';
       } else {
-        return 'player';
+        return 'player_1';
       }
-    } else if (p === 'scissors') {
-      if (c === 'rock') {
-        return 'computer';
+    } else if (p1 === 'scissors') {
+      if (p2 === 'rock') {
+        return 'player_2';
       } else {
-        return 'player';
+        return 'player_1';
       }
     }
   }
@@ -50,41 +59,43 @@ function getComputerChoice(){
     }
 }
 
-function showWinner(winner,computerChoice){
-    if(winner==='player'){
-        scoreboard.player++;
+function showWinner(winner,player_1_Choice,player_2_Choice){
+    if(winner==='player_1'){
+        scoreboard.player_1++;
         result.innerHTML=`
-        <h1 class="text-win">You Win</h1>
-        <i class="fas fa-hand-${computerChoice} fa-10px"></i>
-        <p>Computer chose <strong>${computerChoice.charAt(0).toUpperCase()+computerChoice.slice(1)}  </strong> </p>
+        <h1 class="text-win">Player 1 won </h1>
+        <i class="fas fa-hand-${player_2_Choice} fa-10px"></i>
+        <p>Player 2 chose <strong>${player_2_Choice.charAt(0).toUpperCase()+player_2_Choice.slice(1)}  </strong> </p>
+        
         `;
-    }else if(winner==='computer'){
+    }else if(winner==='player_2'){
         scoreboard.computer++;
         result.innerHTML=`
-        <h1 class="text-lose">You Lose</h1>
-        <i class="fas fa-hand-${computerChoice} fa-10px"></i>
-        <p>Computer chose <strong>${computerChoice.charAt(0).toUpperCase()+computerChoice.slice(1)}  </strong> </p>
+        <h1 class="text-lose">Player 2 won </h1>
+        <i class="fas fa-hand-${player_1_Choice} fa-10px"></i>
+        <p>Player 1 chose <strong>${player_1_Choice.charAt(0).toUpperCase()+player_1_Choice.slice(1)}  </strong> </p>
         `;
     }else{
         result.innerHTML=`
         <h1>It's a draw</h1>
-        <i class="fas fa-hand-${computerChoice} fa-10px"></i>
-        <p>Computer chose <strong>${computerChoice.charAt(0).toUpperCase()+computerChoice.slice(1)}  </strong> </p>
+        <i class="fas fa-hand-${player_2_Choice} fa-10px"></i>
+        <p>Player 2 chose <strong>${player_2_Choice.charAt(0).toUpperCase()+player_2_Choice.slice(1)}  </strong> </p>
         `;
     }
     score.innerHTML=`
-    <p>Player:${scoreboard.player}</p>
-    <p>Computer:${scoreboard.computer}</p>
+    <p>Player1 : ${scoreboard.player_1}</p>
+    <p>Computer:${scoreboard.player_2}</p>
     `;
     modal.style.display='block';
 }
 
 function restartGame(){
-    scoreboard.play=0;
-    scoreboard.computer=0;
+    scoreboard.player_1=0;
+    scoreboard.player_2=0;
     score.innerHTML=`
-    <p><Player: 0 /p>
-    <p><Computer: 0 /p>
+    <p>Player 1 : 0</p>
+    <p>Player 2 : 0</p>
+    
     `;
 
 
@@ -98,6 +109,7 @@ function cleaModel(e){
 }
 
 
+choices.forEach(choice => choice.addEventListener('click',play));
 choices.forEach(choice => choice.addEventListener('click',play));
 window.addEventListener('click',cleaModel);
 restart.addEventListener('click',restartGame);
